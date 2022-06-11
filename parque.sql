c1f7f64179d63d59799484134f6de1c5851a9c59
GiselleRoa <107308397+GiselleRoa@users.noreply.github.com>
2022-06-11 09:41:52 -0400

    Update parque.sql

--- parque.sql
+++ parque.sql
@@ -1,4 +1,4 @@
-CREATE TABLE cliente( 
+CREATE TABLE clientes( 
     rut_cliente NUMBER(11) NOT NULL,  
     dv CHAR(1) NOT NULL, 
     primer_nombre VARCHAR2(250) NOT NULL, 
@@ -10,9 +10,14 @@ CREATE TABLE cliente(
     correo VARCHAR2(250) NOT NULL, 
     comuna_id NUMBER NOT NULL 
 )
+ALTER TABLE "CLIENTE" add CONSTRAINT cliente_pk PRIMARY KEY (rut_cliente); 
 
 CREATE TABLE Comuna( 
     id_comuna NUMBER NOT NULL,  
     nombre VARCHAR(250) NOT NULL, 
     id_provincia NUMBER NOT NULL, 
 );
+ALTER TABLE "Comuna" add CONSTRAINT comuna_pk PRIMARY KEY (id_comuna);
+
+ALTER TABLE "Clientes" add CONSTRAINT comuna_pk PRIMARY KEY (id_comuna);
+REFERENCES Comunas (id_comuna);
